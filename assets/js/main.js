413d28b301b9b0d2db7ad2d3d4799651a032cfb0
rwithik <rwithik@gmail.com>
2019-10-21 13:35:03 +0530

    Search box, navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 // API_ROOT = 'http://localhost:3000/'
 API_ROOT = 'https://evening-river-46814.herokuapp.com/'
+// const search_box = ;
+
 
 const getQuote = () => {
   axios.get(API_ROOT + 'quote',{
@@ -17,3 +19,24 @@ const getQuote = () => {
 }
 
 getQuote()
+
+// document.getElementsByClassName('search-box')[0].addEventListener("keydown", function(event) {
+//     if (event.key === "Enter") {
+//       query = document.getElementsByClassName('search-box')[0].value
+//         url = "https://www.google.com/search?q=" + query;
+//         console.log(url);
+//         // window.open(url, "_self")
+//         location.href = url;
+//     }
+// });
+
+const search = () => {
+  // if (e.which == 13){
+  query = document.getElementsByClassName('search-box')[0].value
+  url = "https://www.google.com/search?q=" + query;
+  console.log(url);
+  // window.open(url, "_self")
+  location.href = url;
+  // document.getElementsByClassName('search-box')[0].action = url;
+// }
+}
