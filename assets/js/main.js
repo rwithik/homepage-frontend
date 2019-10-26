b7669ae93ee84712e36f5ef77e94c644f40c2451
Rwithik Manoj <thoriumnitride@gmail.com>
2019-10-26 10:31:27 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,24 +19,3 @@ const getQuote = () => {
 }
 
 getQuote()
-
-// document.getElementsByClassName('search-box')[0].addEventListener("keydown", function(event) {
-//     if (event.key === "Enter") {
-//       query = document.getElementsByClassName('search-box')[0].value
-//         url = "https://www.google.com/search?q=" + query;
-//         console.log(url);
-//         // window.open(url, "_self")
-//         location.href = url;
-//     }
-// });
-
-const search = () => {
-  // if (e.which == 13){
-  query = document.getElementsByClassName('search-box')[0].value
-  url = "https://www.google.com/search?q=" + query;
-  console.log(url);
-  // window.open(url, "_self")
-  location.href = url;
-  // document.getElementsByClassName('search-box')[0].action = url;
-// }
-}
