374074545445af66ab55cf84d5cba912e2f1352f
rwithik <rwithik@gmail.com>
2019-12-17 15:37:06 +0530

    input box changes and gh pages inout box

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,28 @@
 // API_ROOT = 'http://localhost:3000/'
-API_ROOT = 'https://evening-river-46814.herokuapp.com/';
+const API_ROOT = "https://evening-river-46814.herokuapp.com/";
+const GITHUB_USERNAME = "rwithik";
 
 const getQuote = () => {
-  axios.get(API_ROOT + 'quote',{
-    headers: {
-        'Content-Type': 'application/json'
+  axios
+    .get(API_ROOT + "quote", {
+      headers: {
+        "Content-Type": "application/json"
       }
-  })
-    .then(function (response) {
-      console.log(response);
-      document.getElementById('quote').innerHTML = response.data.quote.fulltext;
     })
-    .catch(function (error) {
+    .then(function(response) {
+      console.log(response);
+      document.getElementById("quote").innerHTML = response.data.quote.fulltext;
+    })
+    .catch(function(error) {
       console.error(error);
     });
-}
+};
 
-getQuote()
+getQuote();
+
+document
+  .querySelector(".input-div.gh-pages-div>form")
+  .addEventListener("submit", e => {
+    e.preventDefault();
+    document.location = `https://${GITHUB_USERNAME}.github.io/${e.target[0].value}`;
+  });
